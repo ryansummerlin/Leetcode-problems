d206a19235a3ef74a33593e6e82900c5f28a583b
ryansummerlin <ryansummerlin777@gmail.com>
2023-10-11 18:55:29 -0700

    finished 1865 finding pairs with a certain sum

--- 1865-finding-pairs-with-a-certain-sum.js
+++ 1865-finding-pairs-with-a-certain-sum.js
@@ -10,6 +10,59 @@
 
 
 
+
+// This is the correct way to do it. Basically you set up a hash map and solve it in the same way as some of these other sum problems
+// where you take the total - the current value and check to see if it exists in the hash map. Time complexity here drops to O(n)
+// instead of O(n * m) to cycle through both arrays and check every value.
+
+var FindSumPairs = function(nums1, nums2) {
+    this.arr1 = nums1;
+    this.arr2 = nums2;
+
+    this.hash = {};
+    for (let i = 0; i < this.arr2.length; i++) {
+        let num = this.arr2[i];
+        if (!this.hash[num]) {
+            this.hash[num] = 1;
+        } else {
+            this.hash[num] += 1;
+        }
+    }
+};
+
+/**
+ * @param {number} index
+ * @param {number} val
+ * @return {void}
+ */
+FindSumPairs.prototype.add = function(index, val) {
+    this.hash[this.arr2[index]] -= 1;
+    this.arr2[index] += val;
+    if (!this.hash[this.arr2[index]]) {
+        this.hash[this.arr2[index]] = 1;
+    } else {
+        this.hash[this.arr2[index]] += 1;
+    }
+
+};
+
+/**
+ * @param {number} tot
+ * @return {number}
+ */
+FindSumPairs.prototype.count = function(tot) {
+    let count = 0;
+
+    for (let i = 0; i < this.arr1.length; i++) {
+        let val = tot - this.arr1[i];
+        if (this.hash[val]) count += this.hash[val];
+    }
+
+    return count;
+};
+
+
+
 // I'm timing out on this one when I tried to brute force it below but I guess it's just a hash map thing. I'll take a look
 // later and try to sort it out. Seems like you have some sort of object/map to represent arr2 which lets you just cycle through
 // arr1 when trying to find the sum. Similar to some other sum problems I've seen here before actually.
